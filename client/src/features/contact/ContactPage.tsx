dda65253235d2bcbec1fcd1e4bc95e721911a691
Sebastian Teisen <sebastian@freshsupplyco.com>
2024-04-01 20:53:59 +1000

    End of section 4

---
+++ client/src/features/contact/ContactPage.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+
+export default function ContectPage() {
+    return (
+        <Typography variant="h2">
+            Contect page
+
+        </Typography>
+    )
+}
