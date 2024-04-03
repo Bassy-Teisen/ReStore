3f3a6eb1dd14c803e6be9fd21a8bcb54fa7d447d
Sebastian Teisen <sebastian@freshsupplyco.com>
2024-04-03 21:34:28 +1000

    End of section 5

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -5,16 +5,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
         private readonly StoreContext _context;
         public ProductsController(StoreContext context)
         {
             _context = context;
-            
-        }   
+
+        }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
@@ -26,7 +24,11 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null) return NotFound();
+
+            return product;
         }
     }
 }
