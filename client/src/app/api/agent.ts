b02ceb3d7d17fb01dad64eeee3aaf609d39f3e86
Sebastian Teisen <sebastian@freshsupplyco.com>
2024-04-07 12:04:53 +1000

    End of section 6

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -2,43 +2,45 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { router } from "../router/routes";
 
-const sleep = () => new Promise(resolve => setTimeout(resolve, 500))
-
+const sleep = () => new Promise((resolve) => setTimeout(resolve, 500));
 
 axios.defaults.baseURL = "http://localhost:5000/api/";
+axios.defaults.withCredentials = true;
 
 const responseBody = (response: AxiosResponse) => response.data;
 
-axios.interceptors.response.use(async response => {
+axios.interceptors.response.use(
+  async (response) => {
     await sleep();
-    return response
-}, (error: AxiosError) => {
-    
-  const {data, status} = error.response as AxiosResponse;
-  switch (status) {
-    case 400:
-      if (data.errors){
-        const modelStateErrors: string[] = [];
-        for (const key in data.errors) {
-          if (data.errors[key]){
-            modelStateErrors.push(data.errors[key])
+    return response;
+  },
+  (error: AxiosError) => {
+    const { data, status } = error.response as AxiosResponse;
+    switch (status) {
+      case 400:
+        if (data.errors) {
+          const modelStateErrors: string[] = [];
+          for (const key in data.errors) {
+            if (data.errors[key]) {
+              modelStateErrors.push(data.errors[key]);
+            }
           }
+          throw modelStateErrors.flat();
         }
-        throw modelStateErrors.flat();
-      }
-      toast.error(data.title);
-      break;
-    case 401:
-      toast.error(data.title);
-      break;
-    case 500:
-      router.navigate('/server-error', {state: {error: data}});
-      break;
-    default:
-      break;
-  }
+        toast.error(data.title);
+        break;
+      case 401:
+        toast.error(data.title);
+        break;
+      case 500:
+        router.navigate("/server-error", { state: { error: data } });
+        break;
+      default:
+        break;
+    }
     return Promise.reject(error.response);
-})
+  }
+);
 
 const request = {
   get: (url: string) => axios.get(url).then(responseBody),
@@ -53,16 +55,23 @@ const Catalog = {
 };
 
 const TestErrors = {
-    get400Error: () => request.get('buggy/bad-request'),
-    get401Error: () => request.get('buggy/unauthorized'),
-    get404Error: () => request.get('buggy/not-found'),
-    get500Error: () => request.get('buggy/server-error'),
-    getValidationError: () => request.get('buggy/validation-error'),
-}
+  get400Error: () => request.get("buggy/bad-request"),
+  get401Error: () => request.get("buggy/unauthorized"),
+  get404Error: () => request.get("buggy/not-found"),
+  get500Error: () => request.get("buggy/server-error"),
+  getValidationError: () => request.get("buggy/validation-error"),
+};
+
+export const Basket = {
+  get: () => request.get("basket"),
+  addItem: (productId: number, quantity = 1) => request.post(`basket?productId=${productId}&quantity=${quantity}`, {}),
+  removeItem: (productId: number, quantity = 1) => request.delete(`basket?productId=${productId}&quantity=${quantity}`)
+};
 
 const agent = {
-    Catalog,
-    TestErrors
-}
+  Catalog,
+  TestErrors,
+  Basket
+};
 
 export default agent;
--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./Header";
-import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { Container, CssBaseline, ThemeProvider, createTheme  } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { useStoreContext } from "../context/StoreContext";
+import { getCookie } from "../api/util/util";
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 function App() {
+  const {setBasket} = useStoreContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const buyerId = getCookie('buyerId');
+    if (buyerId) {
+      agent.Basket.get()
+        .then(basket => setBasket(basket))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false));
+    } else {
+      setLoading(false);
+    }
+  }, [setBasket])
+
   const [darkMode, setDarkMode] = useState(false);
   const palletType = darkMode ? 'dark' : 'light';
   const theme = createTheme({
@@ -21,6 +40,7 @@ function App() {
     setDarkMode(!darkMode);
   }
 
+  if (loading) return <LoadingComponent message="Initializing app..." />
 
   return (
     <ThemeProvider theme={theme}>
